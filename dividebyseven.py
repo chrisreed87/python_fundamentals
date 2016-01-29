a3765d06f2c6a26dd38d7faf322853c2dacb4ee3
Chris Reed <chrisreed87@gmail.com>
2016-01-29 08:51:25 -0700

    added random responses to incorrect answers

--- dividebyseven.py
+++ dividebyseven.py
@@ -1,5 +1,10 @@
+badanswer = ['False! Try Again','Keep trying','Come on, you can do it']
+import random
+print("Try to guess a number divisible by 7")
 while True:
     response = input()
     if int(response) % 7 == 0:
         print(("Yes! ") + (response) + (" is divisible by 7"))
         break
+    elif int(response) % 7 != 0:
+        print(random.choice(badanswer))
